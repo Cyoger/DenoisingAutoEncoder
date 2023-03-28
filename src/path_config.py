a4fd688acc2923de185f2a3c0b3111a13a2f94bd
Cyoger <akshat1sharma2@gmail.com>
2023-03-28 02:27:22 -0400

    config files cuz this is getting annoying

---
+++ src/path_config.py
@@ -0,0 +1,7 @@
+class train_config():
+    dataset_name = "dataset_01_11_2023"
+    directories = [
+                   ".",
+                   "C:\\Vlad Github\\IDIR\\Datasets\\" + dataset_name,
+                  ]
+    
