0427d903d3417962a56b0cee69e0706d1cd3cc59
Cyoger <akshat1sharma2@gmail.com>
2023-03-28 14:41:08 -0400

    updated archs

--- src/models/dae2/architecture.py
+++ src/models/dae2/architecture.py
@@ -17,29 +17,30 @@ class Network():
         #input = layers.Reshape((128,128))
 
         # Encoder
-        x = layers.Conv2D(32, kernel_size = (3, 3), activation="tanh", padding="same")(input)
+        x = layers.Conv2D(32, kernel_size = (3, 3), activation="relu", padding="same")(input)
         x = layers.MaxPooling2D(pool_size =(2, 2), padding="same")(x)
 
-        x = layers.Conv2D(64, kernel_size = (3, 3), activation="tanh", padding="same")(x)
+        x = layers.Conv2D(64, kernel_size = (3, 3), activation="relu", padding="same")(x)
         x = layers.MaxPooling2D(pool_size = (2, 2), padding="same")(x)
 
 
-        x = layers.Conv2D(128, kernel_size = (3, 3), activation="tanh", padding="same")(x)
+        x = layers.Conv2D(128, kernel_size = (3, 3), activation="relu", padding="same")(x)
 
         # x = layers.Dense(32, activation='relu')(layers.Flatten()(x))
+        x = layers.Conv2D(4,  kernel_size = (3, 3), activation="relu", padding="same")(x)
 
         # x = layers.Dense(32768, activation='relu')(x)
 
         # x = layers.Reshape((32, 32, 32))(x)
         # Decoder
-        #x = layers.Conv2DTranspose(64, kernel_size = (3, 3), strides=2, activation="tanh", padding="same")(x)
-        x = layers.UpSampling2D(size=(2, 2))(x)
-        x = layers.Conv2D(64, kernel_size = (3, 3), activation="tanh", padding="same")(x)
+        x = layers.Conv2DTranspose(128, kernel_size = (3, 3), strides=2, activation="tanh", padding="same")(x)
+        # x = layers.UpSampling2D(size=(2, 2))(x)
+        x = layers.Conv2D(64, kernel_size = (3, 3), activation="relu", padding="same")(x)
 
-        x = layers.UpSampling2D(size=(2, 2))(x)
-        x = layers.Conv2D(32, kernel_size = (3, 3), activation="tanh", padding="same")(x)
+        # x = layers.UpSampling2D(size=(2, 2))(x)
+        x = layers.Conv2D(32, kernel_size = (3, 3), activation="relu", padding="same")(x)
 
-        #x = layers.Conv2DTranspose(32, kernel_size = (3, 3), strides=2, activation="tanh", padding="same")(x)
+        x = layers.Conv2DTranspose(32, kernel_size = (3, 3), strides=2, activation="relu", padding="same")(x)
 
         x = layers.Conv2D(1, kernel_size = (3, 3), activation="tanh", padding="same")(x)
 
